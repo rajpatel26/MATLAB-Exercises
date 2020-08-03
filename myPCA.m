04f7e62d7c7dfd751f47e415b5815063d3ea155a
Raj Patel <45488046+rajpatel2606@users.noreply.github.com>
2020-08-03 15:48:57 +0100

    Update myPCA.m

--- myPCA.m
+++ myPCA.m
@@ -1,11 +1,4 @@
-% CS 5810  -- Programming for data analysis 
-%
-%  Assignment 3 | Prof. Alberto Paccanaro
-% 
-%
-% 
-% Insert BELOW your code 
-% marking id - 88693
+% Insert your code below
 
 function [U,S] = myPCA(X)
 %myPCA function receives a dataset as an input argument and returns two
